733e428e3cffdd9a11d8988301939cf8b3131d06
rprethi <2271821@cmontmorency.qc.ca>
2024-03-01 17:47:17 -0500

    correction du devoir p1.3c

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
 const swiperapp = new Swiper(".swiper-filters", {
-    slidesPerView: 'auto', 
-    spaceBetween: 0,
-    grabCursor: true,
-    freeMode: true, 
+  slidesPerView: "auto",
+  spaceBetween: 0,
+  grabCursor: true,
+  freeMode: true,
 });
 
 const swiperappartworks = new Swiper(".swiper-featured-artworks", {
-    direction: "horizontal",
-    grabCursor: true,
-    slidesPerView: 3,
-    spaceBetween: 10,
-    centeredSlides: true,
+  direction: "horizontal",
+  grabCursor: true,
+  slidesPerView: 2.1,
+  spaceBetween: 5,
+  centeredSlides: false,
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const likeButton = document.querySelector(".bi-heart-fill");
+  likeButton.addEventListener("click", function () {
+    likeButton.classList.toggle("liked");
+  });
 });
